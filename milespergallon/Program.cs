b61ebac1cf174bd7402ede4840f98fc0c8dae0af
drkoontz <67968375+drkoontz@users.noreply.github.com>
2020-08-30 14:18:14 -0400

    * .gitignore: set up initial files
    * milespergallon.sln:set up initial files
    * Program.cs:set up initial files
    * milespergallon.csproj: set up initial files

---
+++ milespergallon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace milespergallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
